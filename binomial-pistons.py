2d533623c2db43b5d212f6b565ad1b9b07a06f38
mz0 <mz0@outlook.com>
2025-11-21 16:42:46 +0400

    Binomial distribution (defect rate) - print "n choose k" (n=10)

--- binomial-pistons.py
+++ binomial-pistons.py
@@ -1,3 +1,4 @@
+#! /bin/env python3
 """
 A manufacturer of metal pistons finds that,
 on average, 12% of the pistons they manufacture are rejected
@@ -10,8 +11,7 @@ import math
 
 # Binomial coefficient (n/k) "n choose k" - number of B/G birth order patterns
 # Evaluates to n! / (k! * (n - k)!). Added in version 3.8
-c2o10 = math.comb(10, 2)  # 45
-c8o10 = c2o10
+c8o10 = math.comb(10, 2)  # 45
 c9o10 = 10
 
 defect_rate = 12/100
@@ -22,3 +22,9 @@ good10 = nr ** 10                           # 0.278
 
 print(f'{good8 + good9 + good10:.3f}')  # 0.891
 print(f'{1 - good9 - good10:.3f}')      # 0.342
+
+n = 10
+for k in range(1, n):
+  print(f'{n} choose {k} - {math.comb(n, k)}')
+# 10  45 120 210 252 210 ...
+#  1   2   3   4   5   6 ...
